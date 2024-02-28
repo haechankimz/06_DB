fddc46de3089f5e21cbe166fa6f6bd951031435f
haechankim <haechan0202@naver.com>
2024-02-28 17:48:29 +0900

    20240228 수업내용

--- WORKBOOK/WORKBOOK-1번.sql
+++ WORKBOOK/WORKBOOK-1번.sql
@@ -65,7 +65,7 @@ WHERE STUDENT_NO LIKE 'A21%'
 AND STUDENT_ADDRESS LIKE '%전주%'
 AND ABSENCE_YN = 'N';
 
-
+----------------------------------------------------------------------------
 -- 1번
 -- 영어영문학과(학과코드 002) 학생들의 학번과 이름, 입학 년도를
 -- 입학 년도가 빠른 순으로 표시하는 SQL을 작성하시오.
@@ -88,8 +88,11 @@ ORDER BY PROFESSOR_NAME DESC;
 -- 춘 기술대학교의 남자 교수들의 이름과 나이를 나이 오름차순으로 조회하시오.
 -- (단, 교수 중 2000년 이후 출생자는 없으며 출력 헤더는 "교수이름"으로 한다.
 -- 나이는 '만'으로 계산한다.)
-SELECT PROFESSOR_NAME, PROFESSOR_SSN
-FROM TB_PROFESSOR;
+SELECT PROFESSOR_NAME 교수이름,
+	 FLOOR(MONTHS_BETWEEN(SYSDATE, TO_DATE(19 || SUBSTR(PROFESSOR_SSN, 1, 6))) /12) 나이 
+FROM TB_PROFESSOR
+WHERE SUBSTR(PROFESSOR_SSN, 8, 1) = '1' 
+ORDER BY 나이;
 
 
 -- 4번
@@ -99,11 +102,25 @@ SELECT SUBSTR(PROFESSOR_NAME, 2) 이름
 FROM TB_PROFESSOR; 
 
 
+-- 이름이 4글자 인 경우
+SELECT 
+	CASE 
+		WHEN LENGTH(PROFESSOR_NAME) = 3
+		THEN SUBSTR(PROFESSOR_NAME, 2)
+		
+		WHEN LENGTH(PROFESSOR_NAME) = 4
+		THEN SUBSTR(PROFESSOR_NAME, 3)
+	END 이름
+FROM TB_PROFESSOR;
+
+
 -- 5번
 -- 춘 기술대학교의 재수생 입학자를 조회하시오.
 -- (19살에 입학하면 재수를 하지 않은 것!)
 SELECT STUDENT_NO, STUDENT_NAME
-FROM TB_STUDENT;
+FROM TB_STUDENT
+WHERE EXTRACT(YEAR FROM ENTRANCE_DATE) 
+			- TO_NUMBER('19' || SUBSTR(STUDENT_SSN, 1, 2)) > 19;
 
 
 -- 6번
@@ -121,6 +138,59 @@ WHERE EXTRACT(YEAR FROM ENTRANCE_DATE) < 2000;
 SELECT ROUND(AVG(POINT), 1) 평점
 FROM TB_GRADE
 WHERE STUDENT_NO = 'A517178';
+
+
+-- 8번
+-- 학과별 학생 수를 구하여 "학과번호", "학생수(명)"의 형태로 조회하시오.
+SELECT DEPARTMENT_NO 학과번호, CAPACITY "학생수(명)"
+FROM TB_DEPARTMENT
+GROUP BY DEPARTMENT_NO, CAPACITY;
+
+-- 9번
+-- 지도 교수를 배정받지 못한 학생의 수를 조회하시오
+SELECT COUNT(STUDENT_NO) "COUNT(*)"
+FROM TB_STUDENT 
+WHERE COACH_PROFESSOR_NO IS NULL;
+
+-- 10번
+-- 학번이 A112113인 김고운 학생의 년도 별 평점을 구하는 SQL문을 작성하시오.
+-- 단, 이때 출력화면의 헤더는 "년도", "년도 별 평점"이라고 찍히게 하고, 
+-- 점수는 반올림하여 소수점 이하 한자리까지만 표시한다.
+SELECT SUBSTR(TERM_NO, 1, 4) 년도, ROUND(AVG(POINT), 1) "년도 별 평점"
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A112113'
+GROUP BY SUBSTR(TERM_NO, 1, 4)
+ORDER BY SUBSTR(TERM_NO, 1, 4);
+
+
+-- 11번
+-- 학과 별 휴학생 수를 파악하고자 한다. 
+-- 학과 번호와 휴학생 수를 조회하는 SQL을 작성하시오.
+SELECT DEPARTMENT_NO 학과코드명, COUNT(*)
+FROM TB_STUDENT
+WHERE ABSENCE_YN = 'Y'
+GROUP BY DEPARTMENT_NO
+ORDER BY 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
